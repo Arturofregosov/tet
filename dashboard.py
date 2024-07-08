770fa8e1a18b025ad08c9b2b34cc78f26afd7ca5
Arturofregosov <66340175+Arturofregosov@users.noreply.github.com>
2024-07-08 14:47:43 -0600

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -24,3 +24,23 @@ data = data.groupby(selected_option).sum()["Sales"].reset_index()
 fig = px.bar(data, group_col, "Sales",color_discrete_sequence =['red'])
 config = {'displayModeBar': False}
 st.plotly_chart(fig, config=config)
+
+#Second plot
+options = ["Ship Mode","Sub-Category"]
+selected_option = st.selectbox("", options)
+
+
+# Data aggregation based on user selection
+if selected_option == "Ship Mode":
+    group_col = "Ship Mode"
+else: 
+    group_col = "Sub-Category"
+
+#Group the data by Selected_option
+data = data.groupby(selected_option).sum()["Sales"].reset_index()
+
+# Create bar chart
+fig = px.bar(data, group_col, "Sales",color_discrete_sequence =['green'],orientation='h')
+config = {'displayModeBar': False}
+st.plotly_chart(fig, config=config)
+
